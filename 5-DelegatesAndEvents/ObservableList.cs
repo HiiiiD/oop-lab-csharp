61ed24a2b4df3410a93d2527087e78ade52e21c8
HiiD <salomone.marco@outlook.it>
2020-12-14 13:29:51 +0100

    Finished the 5th exercise

--- 5-DelegatesAndEvents/ObservableList.cs
+++ 5-DelegatesAndEvents/ObservableList.cs
@@ -16,12 +16,14 @@ namespace DelegatesAndEvents
         /// <inheritdoc cref="IObservableList{T}.ElementChanged" />
         public event ListElementChangeCallback<TItem> ElementChanged;
 
+        private readonly List<TItem> _innerList = new List<TItem>();
+
         /// <inheritdoc cref="ICollection{T}.Count" />
         public int Count
         {
             get
             {
-                throw new NotImplementedException();
+                return _innerList.Count;
             }
         }
 
@@ -30,96 +32,101 @@ namespace DelegatesAndEvents
         {
             get
             {
-                throw new NotImplementedException();
+                return ((ICollection<TItem>)_innerList).IsReadOnly;
             }
         }
 
         /// <inheritdoc cref="IList{T}.this" />
         public TItem this[int index]
         {
-            get { throw new System.NotImplementedException(); }
-            set { throw new System.NotImplementedException(); }
+            get => _innerList[index];
+            set => this.Insert(index, value);
         }
 
         /// <inheritdoc cref="IEnumerable{T}.GetEnumerator" />
         public IEnumerator<TItem> GetEnumerator()
         {
-            throw new System.NotImplementedException();
+            return _innerList.GetEnumerator();
         }
 
         /// <inheritdoc cref="IEnumerable.GetEnumerator" />
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return this.GetEnumerator();
+            return GetEnumerator();
         }
 
         /// <inheritdoc cref="ICollection{T}.Add" />
         public void Add(TItem item)
         {
-            throw new System.NotImplementedException();
+            _innerList.Add(item);
+            ElementInserted?.Invoke(this, item, _innerList.Count - 1);
         }
 
         /// <inheritdoc cref="ICollection{T}.Clear" />
         public void Clear()
         {
-            throw new System.NotImplementedException();
+            _innerList.Clear();
         }
 
         /// <inheritdoc cref="ICollection{T}.Contains" />
         public bool Contains(TItem item)
         {
-            throw new System.NotImplementedException();
+            return _innerList.Contains(item);
         }
 
         /// <inheritdoc cref="ICollection{T}.CopyTo" />
         public void CopyTo(TItem[] array, int arrayIndex)
         {
-            throw new System.NotImplementedException();
+            _innerList.CopyTo(array, arrayIndex);
         }
 
         /// <inheritdoc cref="ICollection{T}.Remove" />
         public bool Remove(TItem item)
         {
-            throw new System.NotImplementedException();
+            int index = _innerList.IndexOf(item);
+            bool result = _innerList.Remove(item);
+            ElementRemoved?.Invoke(this, item, index);
+            return result;
         }
 
         /// <inheritdoc cref="IList{T}.IndexOf" />
         public int IndexOf(TItem item)
         {
-            throw new System.NotImplementedException();
+            return _innerList.IndexOf(item);
         }
 
         /// <inheritdoc cref="IList{T}.RemoveAt" />
         public void Insert(int index, TItem item)
         {
-            throw new System.NotImplementedException();
+            TItem oldItem = _innerList[index];
+            _innerList.Insert(index, item);
+            ElementChanged?.Invoke(this, item, oldItem, index);
         }
 
         /// <inheritdoc cref="IList{T}.RemoveAt" />
         public void RemoveAt(int index)
         {
-            throw new System.NotImplementedException();
+            TItem item = _innerList[index];
+            _innerList.RemoveAt(index);
+            ElementRemoved?.Invoke(this, item, index);
         }
 
         /// <inheritdoc cref="object.Equals(object?)" />
         public override bool Equals(object obj)
         {
-            // TODO improve
-            return base.Equals(obj);
+            return _innerList.Equals(obj);
         }
 
         /// <inheritdoc cref="object.GetHashCode" />
         public override int GetHashCode()
         {
-            // TODO improve
-            return base.GetHashCode();
+            return _innerList.GetHashCode();
         }
 
         /// <inheritdoc cref="object.ToString" />
         public override string ToString()
         {
-            // TODO improve
-            return base.ToString();
+            return _innerList.ToString();
         }
     }
 }
