ec9ec03faea30354f6a295783201cc592a800dd6
HiiD <salomone.marco@outlook.it>
2020-12-14 13:07:14 +0100

    Finished exercise 4

--- 4-ExtensionMethods/Complex.cs
+++ 4-ExtensionMethods/Complex.cs
@@ -3,7 +3,7 @@ namespace ExtensionMethods
     using System;
 
     /// <inheritdoc cref="IComplex"/>
-    public class Complex : IComplex
+    public class Complex : IComplex, IEquatable<Complex>
     {
         private readonly double re;
         private readonly double im;
@@ -20,66 +20,74 @@ namespace ExtensionMethods
         }
 
         /// <inheritdoc cref="IComplex.Real"/>
-        public double Real
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Real => this.re;
 
         /// <inheritdoc cref="IComplex.Imaginary"/>
-        public double Imaginary
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Imaginary => this.im;
 
         /// <inheritdoc cref="IComplex.Modulus"/>
-        public double Modulus
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Modulus => Math.Sqrt(Math.Pow(this.re, 2) + Math.Pow(this.im, 2));
 
         /// <inheritdoc cref="IComplex.Phase"/>
         public double Phase
         {
             get
             {
-                throw new System.NotImplementedException();
+                /// <see cref="https://en.wikipedia.org/wiki/Complex_number"/>
+                if (this.Real > 0 || this.Imaginary != 0)
+                {
+                   return 2 * Math.Atan(this.Imaginary / (Math.Sqrt(Math.Pow(this.Real, 2) + Math.Pow(this.Imaginary, 2)) + this.Real));
+                }
+                if (this.Real < 0 && this.Imaginary == 0)
+                {
+                    return Math.PI;
+                }
+                return Double.NaN;
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Complex complex)
+            {
+                return this.Equals(complex);
+            }
+            return base.Equals(obj);
+        }
+
+
+        public bool Equals(Complex other)
+        {
+            return other != null &&
+                   this.Real == other.Real &&
+                   this.Imaginary == other.Imaginary;
+        }
+
+        public bool Equals(IComplex other)
+        {
+            return other != null &&
+                   this.Real == other.Real &&
+                   this.Imaginary == other.Imaginary;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Real, Imaginary);
         }
 
         /// <inheritdoc cref="IComplex.ToString"/>
         public override string ToString()
         {
-            // TODO improve
-            return base.ToString();
+            bool hasNonZeroReal = this.Real != 0;
+            bool hasNonZeroImaginary = this.Imaginary != 0;
+
+            return hasNonZeroReal ?
+                (hasNonZeroImaginary ? $"{this.Real} + {this.Imaginary}i" : this.Real.ToString())
+                : (hasNonZeroImaginary ? $"{this.Imaginary}i" : "0");
         }
 
-        /// <inheritdoc cref="IEquatable{T}.Equals(T)"/>
-        public bool Equals(IComplex other)
-        {
-            throw new System.NotImplementedException();
-        }
+        
 
-        /// <inheritdoc cref="object.Equals(object?)"/>
-        public override bool Equals(object obj)
-        {
-            // TODO improve
-            return base.Equals(obj);
-        }
-
-        /// <inheritdoc cref="object.GetHashCode"/>
-        public override int GetHashCode()
-        {
-            // TODO improve
-            return base.GetHashCode();
-        }
+        
     }
 }
